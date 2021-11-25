7cb1951cfd65a11520914c6cbfdcd1e99ecacb62
vignesh-ragavan <69234008+vignesh-ragavan@users.noreply.github.com>
2021-11-25 20:48:50 +0530

    Update pythons.py

--- pythons.py
+++ pythons.py
@@ -4,10 +4,11 @@ import os
 import jenkins
 
 print(sys.argv[1])
-job_name='param'
-s=jenkins.Jenkins('http://localhost:8080/job/param/')
-build_info=s.get_build_info(job_name,build_num)
-build_info_actions=build_info['actions']
-print(build_info_actions)                           
+print(os.getenv('FolderName')
+# job_name='param'
+# s=jenkins.Jenkins('http://localhost:8080/job/param/')
+# build_info=s.get_build_info(job_name,build_num)
+# build_info_actions=build_info['actions']
+# print(build_info_actions)                           
 
 
