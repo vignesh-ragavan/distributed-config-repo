64e337717bf8a42053f1aa12dcd005f0c10519e5
vignesh-ragavan <69234008+vignesh-ragavan@users.noreply.github.com>
2021-11-25 16:45:33 +0530

    Update pythons.py

--- pythons.py
+++ pythons.py
@@ -7,7 +7,7 @@ print(sys.argv[1])
 job_name='param'
 s=jenkins.Jenkins('http://localhost:8080/job/param/')
 build_info=s.get_build_info(job_name,build_num)
-build_info_actions=build_info['actions]
+build_info_actions=build_info['actions']
 print(build_info_actions)                           
 
 
