f4b297644af3056dde5f1562f2411a227ed51a8e
vignesh-ragavan <69234008+vignesh-ragavan@users.noreply.github.com>
2021-11-25 20:57:34 +0530

    Update pythons.py

--- pythons.py
+++ pythons.py
@@ -4,6 +4,7 @@ import os
 
 #print(sys.argv[1])
 print(os.getcwd())
+print(os.getenv("FolderName"))
 # job_name='param'
 # s=jenkins.Jenkins('http://localhost:8080/job/param/')
 # build_info=s.get_build_info(job_name,build_num)
